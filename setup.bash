ec77196bfb0100010f71bb8accc5b7ad6e4ac114
Alexandre Daubricourt <alexandre@daubricourt.com>
2024-12-02 11:46:54 +0100

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,9 +1,9 @@
 read -p "Enter your GitHub token: " token
 
-echo "MR_GITHUB_PAT=\"$token\"" > .env
-
 echo "downloading multi-runners tool"
 git clone https://github.com/vbem/multi-runners.git
+
+echo "MR_GITHUB_PAT=\"$token\"" > multi-runners/.env
 
 echo "adding recreate_all job to crontab"
 (crontab -l 2>/dev/null; echo "0 3 * * SUN bash $(readlink -e recreate_all.bash)") | crontab -
