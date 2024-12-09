200113f9295e83e757a7ea34368627a43f891dc4
Alexandre Daubricourt <alexandre@daubricourt.com>
2024-12-09 14:36:24 +0100

    Update recreate_all.bash

--- recreate_all.bash
+++ recreate_all.bash
@@ -1,5 +1,9 @@
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd "$parent_path"
+
 #!/bin/bash
-for i in {0..7}
+for i in {0..5}
 do
   bash multi-runners/mr.bash del --user runner-$i
   bash multi-runners/mr.bash add --org airporting
