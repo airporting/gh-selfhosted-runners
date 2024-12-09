7e5462c7dca9f06d8b2fa05e5d2e112e086b2f9a
Alexandre Daubricourt <alexandre@daubricourt.com>
2024-12-09 14:45:15 +0100

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -15,3 +15,9 @@ echo "adding recreate_all job to crontab"
 echo "creating runners for the first time"
 bash recreate_all.bash
 
+sudo fallocate -l 8G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
