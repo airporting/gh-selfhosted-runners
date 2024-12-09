ff25bb0d162bd523cbe48ca9f92940b3d87e0a33
Alexandre Daubricourt <alexandre@daubricourt.com>
2024-12-09 14:36:13 +0100

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,3 +1,7 @@
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd "$parent_path"
+
 read -p "Enter your GitHub token: " token
 
 echo "downloading multi-runners tool"
@@ -6,7 +10,8 @@ git clone https://github.com/vbem/multi-runners.git
 echo "MR_GITHUB_PAT=\"$token\"" > multi-runners/.env
 
 echo "adding recreate_all job to crontab"
-(crontab -l 2>/dev/null; echo "0 3 * * SUN bash $(readlink -e recreate_all.bash)") | crontab -
+(crontab -l 2>/dev/null; echo "0 3 * * SUN bash $(readlink -e recreate_all.bash) 2>&1 | logger -t CRON") | crontab -
 
 echo "creating runners for the first time"
 bash recreate_all.bash
+
