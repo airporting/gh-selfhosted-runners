bf972315433e203c0a62c87c1edb8cfd7e0fa671
Alexandre Daubricourt <alexandre@daubricourt.com>
2025-04-24 13:44:33 +0200

    Update recreate_all.bash

--- recreate_all.bash
+++ recreate_all.bash
@@ -5,6 +5,9 @@ cd "$parent_path"
 
 for i in {0..5}
 do
+  LABELS=$(bash ./labels.bash runner-$i)
+  KEEP_LABELS=$(echo $LABELS | jq -r '[.[] | select(startswith("repo"))] | join(",")')
+  echo keeping labels: $KEEP_LABELS
   bash multi-runners/mr.bash del --user runner-$i
-  bash multi-runners/mr.bash add --org airporting
+  bash multi-runners/mr.bash add --org airporting ${KEEP_LABELS:+--labels "$KEEP_LABELS"}
 done
