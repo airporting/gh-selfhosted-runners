140b88d4a0d362ad975b60cd05506b1045b8796c
Mathieu KIM ROBIN <MathRobin@users.noreply.github.com>
2025-06-05 22:48:35 +0200

    Update setup_cron.bash

--- setup_cron.bash
+++ setup_cron.bash
@@ -2,6 +2,6 @@ echo "clearing all previous crontab jobs"
 crontab -r
 
 echo "adding auto git pull for this repo to crontab"
-(crontab -l 2>/dev/null; echo "0 20 * * SUN,WED cd $(readlink -e .) && git pull 2>&1 | logger -t CRON") | crontab -
+(crontab -l 2>/dev/null; echo "0 20 * * * cd $(readlink -e .) && git pull 2>&1 | logger -t CRON") | crontab -
 echo "adding recreate_all job to crontab"
-(crontab -l 2>/dev/null; echo "0 21 * * SUN,WED bash $(readlink -e recreate_all.bash) 2>&1 | logger -t CRON") | crontab -
+(crontab -l 2>/dev/null; echo "0 21 * * SUN,TUE,THU bash $(readlink -e recreate_all.bash) 2>&1 | logger -t CRON") | crontab -
